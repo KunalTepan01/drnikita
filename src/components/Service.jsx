230526c24860c83d98398faf368ee735e85b3441
Kunal <kunaltepan1@gmail.com>
2025-05-31 16:06:33 +0530

    new t3

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -8,7 +8,7 @@ const clinicsData = [
     {
         name: "Orthodontic Services",
         slug: "orthodontic",
-        imageSrc: "/images/orthodontic.jpeg",
+        imageSrc: "/images/Orthodontic.jpeg",
         discription: "Correcting misaligned teeth and jaws using braces, aligners, and other devices for a healthier, straighter smile."
     },
     {
